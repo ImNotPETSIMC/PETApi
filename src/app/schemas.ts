72523c6ba86f714f0183cddce762bb0bf5b9af79
Enzo <enzowederma@gmail.com>
2023-12-12 00:20:16 -0300

    🎈 perf: improve MemberRequestSchema regex
    Changes MemberRequestSchema regex to test for "BSI".

--- src/app/schemas.ts
+++ src/app/schemas.ts
@@ -2,8 +2,7 @@ import Zod from "zod";
 
 export const MemberRequestSchema = Zod.object({
     matricula: Zod
-        .string({ required_error: "Matricula must compose request body."})
-        .length(11, { message: "Matricula must be 11 characters long." })
-        .includes("BSI", {message: ""})
-        .regex(/^3\d\d\d\d[A-Za-z]+[0-9]+$/gm, {message: "Matricula must match UFU's pattern for Information System students."})
+        .string({ required_error: "Field matricula must compose request body."})
+        .length(11, { message: "Field matricula must be 11 characters long." })
+        .regex(/\d\d\d\d\dBSI[0-9]+/i, {message: "Field matricula must match UFU's pattern for Information System students."})
 });
