0b3655c6cb39fc7c858982998ed8a7c8120c33b0
Enzo <enzowederma@gmail.com>
2023-12-13 21:59:56 -0300

    ✨ feat: add MembersRequestSchema for show method
    add MembersRequestSchema to validate show route request body.

--- src/app/schemas.ts
+++ src/app/schemas.ts
@@ -6,3 +6,9 @@ export const MemberRequestSchema = Zod.object({
         .length(11, { message: "Field matricula must be 11 characters long." })
         .regex(/\d\d\d\d\dBSI[0-9]+/i, {message: "Field matricula must match UFU's pattern for Information System students."})
 });
+
+export const MembersRequestSchema = Zod.object({
+    status: Zod
+    .string({ required_error: "Field status must compose request body."})
+    .refine((str => (str == "Petiano" || str == "Ex-Petiano" || str ==  "Tutor" || str ==  "Ex-Tutor")), { message: "Status must be Petiano, Ex-Petiano, Tutor or Ex-Tutor" })
+});
