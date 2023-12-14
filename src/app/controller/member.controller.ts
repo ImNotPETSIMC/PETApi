8793f6244f87d4328f2fa2971ab6afaafb712a54
Enzo <enzowederma@gmail.com>
2023-12-14 16:41:08 -0300

    ✨ feat: add show method request-body validation
    Adds validation for request body in show method from member service.

--- src/app/controller/member.controller.ts
+++ src/app/controller/member.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { ValidationExceptionError } from "../exception/validation.exception";
 import MemberService from "../service/member.service";
-import { MemberRequestSchema } from "../schemas";
+import { MemberRequestSchema, MembersRequestSchema } from "../schemas";
 import { handleZodIssues } from "../helper/handleZodIssues";
 
 export class MembersController {
@@ -21,8 +21,8 @@ export class MembersController {
     }
     
     try {
-
       const { data } = result;
+
       const member = await memberService.search(data.matricula);
 
       res.status(200).send(member);
@@ -32,22 +32,29 @@ export class MembersController {
         res.status(error.code).send({ error: error.message });
         return;
       }
-      
+
       throw error;
     }
   }
 
   public async show(req: Request, res: Response) {
     const memberService = new MemberService();
-
-    const { data } = req.body;
-
-    if (!data || !data.status ) {
+    
+    if (!req.body.data) {
       res.status(422).send({ error: "Missing some fields." });
+      return;
+    }
+    
+    const result = MembersRequestSchema.safeParse(req.body.data);
+    
+    if (!result.success) {
+      res.status(422).send({ errors: result.error.issues.map(handleZodIssues) });
       return;
     }
 
     try {
+      const { data } = result;
+      
       const members = await memberService.show(data.status);
 
       res.status(200).send(members);
