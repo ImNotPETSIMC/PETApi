3ec53a5ea9dd7aba79a587f1e0b08291015a4780
Enzo <enzowederma@gmail.com>
2024-01-01 10:09:24 -0300

    🎈 perf: limit payload size

--- src/app/index.ts
+++ src/app/index.ts
@@ -15,7 +15,7 @@ class App {
 
   middleware() {
     this.app.use(bodyParser.json());
-    this.app.use(express.json());
+    this.app.use(express.json({limit: '50mb'}));
     this.app.use(cors());
   }
 
